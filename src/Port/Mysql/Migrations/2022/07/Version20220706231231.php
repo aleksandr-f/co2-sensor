24662789da891155a1be0bd7184b1915c6356d73
Aleksandr <aleksandr.f.pb@gmail.com>
2022-07-07 01:20:01 +0200

    Added database migration

---
+++ src/Port/Mysql/Migrations/2022/07/Version20220706231231.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Port\Mysql\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220706231231 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE alerts (id VARCHAR(36) NOT NULL, sensor_id VARCHAR(36) NOT NULL, start_time DATETIME NOT NULL, end_time DATETIME DEFAULT NULL, measurements LONGTEXT NOT NULL COMMENT \'(DC2Type:simple_array)\', INDEX sensor_id_idx (sensor_id), INDEX end_time_idx (end_time), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE measurements (id VARCHAR(36) NOT NULL, sensor_id VARCHAR(36) NOT NULL, co2 INT NOT NULL, time DATETIME NOT NULL, INDEX time_idx (time), INDEX sensor_id_idx (sensor_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sensors (id VARCHAR(36) NOT NULL, status VARCHAR(10) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE alerts');
+        $this->addSql('DROP TABLE measurements');
+        $this->addSql('DROP TABLE sensors');
+    }
+}
