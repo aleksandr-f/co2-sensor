16bb3676b7ef7c0da14381c6e29208f2c1d1002c
Aleksandr <aleksandr.f.pb@gmail.com>
2022-09-03 14:28:10 +0200

    SensorRepositoryTest

---
+++ tests/Factories/SensorFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Factories;
+
+use App\Application\CreateSensorCommand;
+use App\Application\SensorRepositoryInterface;
+use App\Domain\Sensor;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class SensorFactory
+{
+    public function __construct(
+        private readonly MessageBusInterface $messageBus,
+        private readonly SensorRepositoryInterface $sensorRepository,
+    ) {
+    }
+
+    public function createOne(array $data): Sensor
+    {
+        $id = $data['id'] ?? 'fe7e5baf-f9d9-41ef-a2ba-9ebfb4cc4667';
+
+        $this->messageBus->dispatch(new CreateSensorCommand(
+            sensorId: $id,
+        ));
+
+        return $this->sensorRepository->get(id: $id);
+    }
+}
---
+++ tests/Integration/Port/Mysql/Repository/SensorRepositoryTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Integration\Port\Mysql\Repository;
+
+use App\Application\SensorNotFoundException;
+use App\Domain\Sensor;
+use App\Port\Mysql\Repository\SensorRepository;
+use Tests\BaseKernelWithDBTestCase;
+use Tests\Factories\SensorFactory;
+
+final class SensorRepositoryTest extends BaseKernelWithDBTestCase
+{
+    private SensorRepository $sensorRepository;
+
+    private SensorFactory $sensorFactory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->sensorRepository = self::getContainer()->get(id: SensorRepository::class);
+
+        $this->sensorFactory = self::getContainer()->get(id: SensorFactory::class);
+    }
+
+    protected function tearDown(): void
+    {
+        unset($this->sensorRepository);
+
+        parent::tearDown();
+    }
+
+    public function testExists(): void
+    {
+        self::assertFalse(
+            condition: $this->sensorRepository->exists(
+                id: '0c9cd912-dcb9-49bf-a260-662a6e090d93',
+            ),
+        );
+
+        $this->sensorFactory->createOne(data: [
+            'id' => '0c9cd912-dcb9-49bf-a260-662a6e090d93',
+        ]);
+
+        self::assertTrue(
+            condition: $this->sensorRepository->exists(
+                id: '0c9cd912-dcb9-49bf-a260-662a6e090d93',
+            ),
+        );
+    }
+
+    public function testGet(): void
+    {
+        $this->expectException(exception: SensorNotFoundException::class);
+
+        $this->sensorRepository->get(id: '0c9cd912-dcb9-49bf-a260-662a6e090d93');
+
+        $this->sensorFactory->createOne(data: [
+            'id' => '0c9cd912-dcb9-49bf-a260-662a6e090d93',
+        ]);
+
+        $this->sensorRepository->get(id: '0c9cd912-dcb9-49bf-a260-662a6e090d93');
+    }
+
+    /** @doesNotPerformAssertions */
+    public function testSave(): void
+    {
+        $sensor = new Sensor(id: '0c9cd912-dcb9-49bf-a260-662a6e090d93');
+
+        $this->sensorRepository->save(sensor: $sensor);
+
+        $this->sensorRepository->get(id: '0c9cd912-dcb9-49bf-a260-662a6e090d93');
+    }
+}
