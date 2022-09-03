ef36cdec5da2f88a838491315b210f61cb4ae9ff
Aleksandr <aleksandr.f.pb@gmail.com>
2022-09-03 14:57:18 +0200

    CreateSensorCommandHandlerTest

---
+++ tests/Unit/Application/CreateSensorCommandHandlerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Application;
+
+use App\Application\CreateSensorCommand;
+use App\Application\CreateSensorCommandHandler;
+use App\Application\SensorRepositoryInterface;
+use PHPUnit\Framework\TestCase;
+
+final class CreateSensorCommandHandlerTest extends TestCase
+{
+    public function testHandler(): void
+    {
+        $repository = $this->createMock(originalClassName: SensorRepositoryInterface::class);
+        $repository
+            ->expects(self::once())
+            ->method(constraint: 'save')
+            ;
+
+        $handler = new CreateSensorCommandHandler(
+            sensorRepository: $repository,
+        );
+
+        $handler->__invoke(command: new CreateSensorCommand('5218eb04-9da6-4dd5-a780-cd50f9378ff6'));
+    }
+}
